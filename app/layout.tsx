bfbab52c12f175307cd42432febf091c5a2a4103
Preeth Ravani <90096033+SorryBoi@users.noreply.github.com>
2025-06-03 20:48:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Recruto.AI',
+  title: 'RecrutoAI',
   description: 'Created for Job Prep',
   generator: 'preeth',
 }
